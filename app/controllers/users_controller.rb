7bee9076b7971f8cf8d5a045ef71d9939e8d35ff
Timo Tyrrell <timomitchel23@gmail.com>
2019-03-15 13:31:01 -0600

    update user controller params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -69,6 +68,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:team_id)
+      params.require(:user).permit(:username, :password)
     end
 end
