f13bf7d8b7c501495ffd4c3ebce6a7f5eb257b4c
Timo Tyrrell <timomitchel23@gmail.com>
2019-03-15 13:33:30 -0600

    Add and update tests to pass

--- spec/features/visitor_can_create_account_spec.rb
+++ spec/features/visitor_can_create_account_spec.rb
@@ -11,10 +11,10 @@ feature "visitor can register" do
 
   expect(current_path).to eq(new_user_path)
 
-  fill_in :user_username, with: username
-  fill_in :user_password, with: "test"
+  fill_in 'user_username', with: username
+  fill_in 'user_password', with: "test"
 
-  click_on "Register"
+  click_on "Create User"
 
   expect(page).to have_content("Welcome, #{username}!")
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of(:username)}
     it {should validate_presence_of(:password)}
   end
+  describe "relationships" do 
+    it "has many teams" do
+      user = build(:user)
+      expect(user).to respond_to(:teams)
+    end
+  end
 end
