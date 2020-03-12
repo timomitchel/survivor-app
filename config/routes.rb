3b95ca794bffccb9e8178c27747a0974948bb7e0
Timo Tyrrell <timomitchel23@gmail.com>
2020-03-12 14:02:20 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root :to =>"welcome#index"
+  post '/user_team', :to => 'userteams#create'
   resources :users
-  root "welcome#index"
-  resources :teams
+  resources :welcome
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
