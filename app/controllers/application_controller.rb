1e8480dcc479e6be8df859f82ced8e1f8e6e647d
Tim Tyrrell <timomitchel23@gmail.com>
2020-09-30 12:24:30 -0600

    Update puma, gems, styles, and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,28 +11,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  def current_week
-    Week.where('start_time < ? AND end_time > ?', Date.today, Date.today)
-        .first
-        .number
-  end
-
-  def current_week_id
-    Week.where('start_time < ? AND end_time > ?', Date.today, Date.today)
-        .first
-        .id
-  end
-
-  def current_season
-    Season.all.first.id
-  end
-
-  def current_pick
-    if  find_current_pick
-      Team.find(find_current_pick.team_id).abbreviation
-    else
-      "No team picked yet"
-    end
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,31 @@
 require 'pry'
 module ApplicationHelper
+  
     def find_current_pick
       @pick ||= UserTeam.find_by(user_id: current_user.id, week_id: current_week_id)
+    end
+
+    def current_week
+      Week.where('start_time < ? AND end_time > ?', Date.today, Date.today)
+          .first
+          .number
+    end
+
+    def current_week_id
+      Week.where('start_time < ? AND end_time > ?', Date.today, Date.today)
+          .first
+          .id
+    end
+
+    def current_season
+      Season.all.first.id
+    end
+
+    def current_pick
+      if  find_current_pick
+        Team.find(find_current_pick.team_id).abbreviation
+      else
+        "No team picked yet"
+      end
     end
 end
