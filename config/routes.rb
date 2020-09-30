9ef4c356eb3ec05f34051cde57c04d65af611519
Tim Tyrrell <timomitchel23@gmail.com>
2020-09-30 11:04:21 -0600

    Add routes and caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root :to =>"welcome#index"
-  post '/user_team', :to => 'userteams#create'
-  resources :users
-  resources :welcome
+  resources :welcome, only: ["index"]
+
+  post '/user_team', :to => 'userteams#create', as: 'make_pick'
+
+  resources :users, only: [:new, :create, :edit, :update, :show]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create', as: "create_session"
+  delete '/session', to: 'sessions#destroy', as: "delete_session"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
